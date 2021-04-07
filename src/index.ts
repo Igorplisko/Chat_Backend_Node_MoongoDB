492a049ac9420032d7f6298800a61c69ae58ed6b
igorplisko <igorplisko95@gmail.com>
2021-04-07 12:02:47 +0300

    main

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,50 @@
 import mongoose from 'mongoose'
 import express from 'express'
+import bodyParser from 'body-parser'
 
 import User from "./schemas/User"
 
 const app = express()
 
 
-mongoose.connect('mongodb://localhost:27017/chat', { useNewUrlParser: true, });
+// app.use(bodyParser.json())
 
-app.get('/', (_: any, res: any) => {
+async function start() {
+   try {
+
+      await mongoose.connect('mongodb://localhost:27017/chat', {
+         useNewUrlParser: true,
+         useUnifiedTopology: true
+      });
+      app.listen(3000, function () {
+         console.log(`Example app listening at http://localhost:3000`)
+      })
+   } catch (e) {
+      console.log(e)
+   }
+}
+
+
+// mongoose.connect('mongodb://localhost:27017/chat', { useNewUrlParser: true, useFindAndModify: false });
+
+
+
+
+app.get('/create', (req: any, res: any) => {
    res.send('Hello World!')
-   const UsersGet = new User({ email: 'Igor@domain.com', fullName: 'Test Igor' });
-   UsersGet.save().then(() => console.log('User created'))
+   const userCreate = new User({ email: 'Igor@domain.com', fullName: 'Test Igor' });
+   userCreate.save().then((obj: any) => {
 
 
+      res.json(obj)
+
+   })
 });
 
 
+start()
 
 
-
-app.listen(3000, function () {
-   console.log(`Example app listening at http://localhost:3000`)
-})
+// app.listen(3000, function () {
+//    console.log(`Example app listening at http://localhost:3000`)
+// })
--- src/schemas/User.ts
+++ src/schemas/User.ts
@@ -1,19 +1,37 @@
 import mongoose, { Schema } from 'mongoose';
+import { isEmail } from "validator";
 
-
-const UserSchema = new Schema({
-   email: String,
-   avatar: String,
-   fullName: String,
-   password: String,
-   confirmed: Boolean,
-   confirm_hash: String,
-   last_seen: Date,
-}, {
-   timestamps: true
-
-
-});
+const UserSchema = new Schema(
+   {
+      email: {
+         type: String,
+         require: "Email address is required",
+         validate: [isEmail, "Invalid email"],
+         unique: true,
+      },
+      fullName: {
+         type: String,
+         required: "Fullname is required",
+      },
+      password: {
+         type: String,
+         required: "Password is required",
+      },
+      confirmed: {
+         type: Boolean,
+         default: false,
+      },
+      avatar: String,
+      confirm_hash: String,
+      last_seen: {
+         type: Date,
+         default: new Date(),
+      },
+   },
+   {
+      timestamps: true,
+   }
+);
 
 const User = mongoose.model("User", UserSchema)
 
